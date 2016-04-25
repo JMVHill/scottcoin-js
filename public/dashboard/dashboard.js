213e2535488dd7d1ffd5853d88ec8b5a8ea828d2
JMVHill <james.hill@lcsltd.co.uk>
2016-04-25 13:14:02 +0100

    Addded block inspector and generic(ised) inspector container

--- public/dashboard/dashboard.js
+++ public/dashboard/dashboard.js
@@ -11,142 +11,171 @@ angular.module('scApp.dashboard', ['ngRoute'])
 
 .controller('DashboardCtrl', ['$scope', function($scope) {
 
-	// Define core variables
-	$scope.blocks 				= [];
-	$scope.transactions 		= [];
-	$scope.selectedTx			= null;
-	$scope.selectedBlock		= null;
-	$scope.selectedTxKeyValue	= [];
+    // Define core variables
+    $scope.blocks                   = [];
+    $scope.transactions             = [];
+    $scope.selectedTx               = null;
+    $scope.selectedBlock            = null;
+    $scope.selectedTxKeyValue       = [];
+    $scope.selectedBlockKeyValue    = [];
 
-	// Define constant variables
-	$scope.MAX_TX 				= 8;
-	$scope.MAX_BLOCKS 			= 6;
+    // Define constant variables
+    $scope.MAX_TX                   = 8;
+    $scope.MAX_BLOCKS               = 6;
 
-	// Construct socket object
-	var socketio = io();
+    // Define 'constants'
+    var txKeys                      = ["account", "address", "amount", "blockhash", "blockindex", "blocktime",
+                                       "catagory", "confirmations", "generated", "time", "timereceived", "vout"];
+    var blockKeys                   = ["bits", "chainwork", "confirmations", "difficulty", "height", "merkleroot",
+                                       "nonce", "previousblockhash", "signature", "size", "time", "tx", "version"];
 
-	// Utility function to merge lists
-	function mergeNewItemsToList(list, maxSize, newList) {
-		var resultList = list;
-		for (var index = 0; index < Math.min(newList.length, maxSize); index ++) {
-			resultList.unshift(newList[index]);
-		}
-		resultList.splice(maxSize);
-		return resultList;
-	}
+    // Construct socket object
+    var socketio = io();
 
-	// Display output functions
-	$scope.getHashDisplayText = function(hash, length) {
-		return hash.substring(0, length) + "...";
-	}
+    // Utility function to merge lists
+    function mergeNewItemsToList(list, maxSize, newList) {
+        var resultList = list;
+        for (var index = 0; index < Math.min(newList.length, maxSize); index ++) {
+            resultList.unshift(newList[index]);
+        }
+        resultList.splice(maxSize);
+        return resultList;
+    }
 
-	// Utility function to check if a string is blacklisted
-	function blackListedString(key) {
-		var blackList = ["$$hashKey", "walletconflicts", "txid"];
-		for (var index = 0; index < blackList.length; index ++) {
-			if (blackList[index] == key) {
-				return true;
-			}
-		}
-		return false;
-	}
+    // Get mode for working panel
+    $scope.viewerMode = function(modeCheck) {
+        return (modeCheck == "transactionInspect" &&
+                    $scope.selectedTx &&
+                    !$scope.selectedBlock) ||
+               (modeCheck == "blockInspect" &&
+                    !$scope.selectedTx &&
+                    $scope.selectedBlock) ||
+               (modeCheck == "nothingSelected" &&
+                    !$scope.selectedTx &&
+                    !$scope.selectedBlock);
+    }
 
-	// Return elapsed time between now and value
-	$scope.elapsedTime = function(value) {
-		var nowTime = new Date().getTime();
-		var dateConvert = new Date(value * 1000);
-		dateConvert = new Date(nowTime - dateConvert);
-		return dateConvert;
-	}
-	$scope.formatDate = function(date) {
-		var hours = date.getHours();
-		var mins = date.getMinutes();
-		var seconds = date.getSeconds();
-		return (hours.toString().length == 1 ? "0" + hours : hours) + ":" + 
-		       (mins.toString().length == 1 ? "0" + mins : mins) + ":" + 
-		       (seconds.toString().length == 1 ? "0" + seconds : seconds);
-	}
+    // Display output functions
+    $scope.getHashDisplayText = function(hash, length) {
+        return hash.substring(0, length) + "...";
+    }
 
-	// Format value depending on key
-	function createValueForKey(key, value) {
-		var dateConvert;
-		if (key == "time" ||
-			key == "timereceived" ||
-			key == "blocktime") {
-			value = new Date(value * 1000);
-		}
-		return {
-			key: key.charAt(0).toUpperCase() + key.slice(1),
-			value: value
-		};
-	}
+    // Return elapsed time between now and value
+    $scope.elapsedTime = function(value) {
+        var nowTime = new Date().getTime();
+        var dateConvert = new Date(value * 1000);
+        dateConvert = new Date(nowTime - dateConvert);
+        return dateConvert;
+    }
+    $scope.formatDate = function(date) {
+        var hours = date.getHours();
+        var mins = date.getMinutes();
+        var seconds = date.getSeconds();
+        return (hours.toString().length == 1 ? "0" + hours : hours) + ":" + 
+               (mins.toString().length == 1 ? "0" + mins : mins) + ":" + 
+               (seconds.toString().length == 1 ? "0" + seconds : seconds);
+    }
 
-	// Set selected transaction
-	$scope.transactionClick = function(tx) {
-		$scope.selectedTx = tx;
-		$scope.selectedTxKeyValue = [];
-		for (var key in tx) {
-			if (tx.hasOwnProperty(key) &&
-				!blackListedString(key) &&
-				tx[key]) {
-				$scope.selectedTxKeyValue.push(createValueForKey(key, tx[key]));
-			}
-		}
-		console.log($scope.selectedTxKeyValue);
-	}
+    // Format value depending on key
+    function createValueForKey(key, value) {
+        var dateConvert;
+        if (key == "time" ||
+            key == "timereceived" ||
+            key == "blocktime") {
+            value = new Date(value * 1000);
+        }
+        return {
+            key: key.charAt(0).toUpperCase() + key.slice(1),
+            value: value
+        };
+    }
+
+    // Utility function to check if a string is blacklisted
+    function blackListedString(key) {
+        var blackList = ["$$hashKey", "walletconflicts", "txid"];
+        for (var index = 0; index < blackList.length; index ++) {
+            if (blackList[index] == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Set selected transaction
+    function resetSelectedItems() {
+        $scope.selectedTx = null;
+        $scope.selectedBlock = null;
+    }
+    function createKeyValueArray(sourceObject, keyArray) {
+        if (!keyArray) { return []; }
+        var resultArray = [];
+        for (var key in sourceObject) {
+            if (sourceObject.hasOwnProperty(key) &&
+                keyArray.indexOf(key) != -1 &&
+                sourceObject[key]) {
+                resultArray.push(createValueForKey(key, sourceObject[key]));
+            }
+        }
+        return resultArray;
+    }
+    $scope.transactionClick = function(tx) {
+        resetSelectedItems();
+        $scope.selectedTx = tx;
+        $scope.selectedTxKeyValue = createKeyValueArray(tx, txKeys);
+    }
+    $scope.blockClick = function(block) {
+        resetSelectedItems();
+        $scope.selectedBlock = block;
+        $scope.selectedBlockKeyValue = createKeyValueArray(block, blockKeys);
+    };
 
 
-	// Setup testing socket events
-	socketio.on('connect', function() {
-		socketio.emit('gettransactions');
-		socketio.emit('getblocks');
-	});
-	socketio.on('newtransactions', function(data) {
-		console.log("New transactions");
-		console.log(data);
-		$scope.transactions = mergeNewItemsToList($scope.transactions, $scope.MAX_TX, data, []);
-		$scope.$apply();
-	});
-	socketio.on('newblocks', function(data) {
-		console.log("New blocks");
-		console.log(data);
-		$scope.blocks = mergeNewItemsToList($scope.blocks, $scope.MAX_BLOCKS, data, []);
-		$scope.$apply();
-	});
-	socketio.on('updatetransactions', function(data) {
-		console.log("Update transactions");
-		console.log(data);
-	});
-	socketio.on('sendtransactions', function(data) {
-		console.log("Received transaction list");
-		console.log(data);
-		$scope.transactions = data;
-		$scope.$apply();
-	});
-	socketio.on('sendblocks', function(data) {
-		console.log("Received block list");
-		console.log(data);
-		$scope.blocks = data;
-		$scope.$apply();
-	});
+    // Setup testing socket events
+    socketio.on('connect', function() {
+        socketio.emit('gettransactions');
+        socketio.emit('getblocks');
+    });
+    socketio.on('newtransactions', function(data) {
+        console.log("New transactions");
+        console.log(data);
+        $scope.transactions = mergeNewItemsToList($scope.transactions, $scope.MAX_TX, data, []);
+        $scope.$apply();
+    });
+    socketio.on('newblocks', function(data) {
+        console.log("New blocks");
+        console.log(data);
+        $scope.blocks = mergeNewItemsToList($scope.blocks, $scope.MAX_BLOCKS, data, []);
+        $scope.$apply();
+    });
+    socketio.on('updatetransactions', function(data) {
+        console.log("Update transactions");
+        console.log(data);
+    });
+    socketio.on('sendtransactions', function(data) {
+        console.log("Received transaction list");
+        console.log(data);
+        $scope.transactions = data;
+        $scope.$apply();
+    });
+    socketio.on('sendblocks', function(data) {
+        console.log("Received block list");
+        console.log(data);
+        $scope.blocks = data;
+        $scope.$apply();
+    });
 
-	// Get a list of transactions on function call
-	$scope.getPullList = function() {
-		socketio.emit('gettransactions');
-		socketio.emit('getblocks');
-		console.log("Requested transaction and block list");
-	};
-	$scope.getPullAll = function() {
-		socketio.emit('alltransactions');
-		socketio.emit('allblocks');
-		console.log("Requested transaction and block list");
-	};
+    // Get a list of transactions on function call
+    $scope.getPullList = function() {
+        socketio.emit('gettransactions');
+        socketio.emit('getblocks');
+        console.log("Requested transaction and block list");
+    };
+    $scope.getPullAll = function() {
+        socketio.emit('alltransactions');
+        socketio.emit('allblocks');
+        console.log("Requested transaction and block list");
+    };
 
-	// Testing methods
-	$scope.clickFunction = function() {
-		console.log("Test");
-	};
-
-	setInterval(function() { $scope.$apply(); }, 1000);
+    setInterval(function() { $scope.$apply(); }, 1000);
 
 }]);
