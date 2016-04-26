328e98c1e7cf8c716b631faaaebf8ad6594c9f2d
jmorris <jmorris@scottlogic.com>
2016-04-26 02:37:42 -0700

    added wallet partial variables

--- public/wallet/wallet.js
+++ public/wallet/wallet.js
@@ -1,59 +1,66 @@
 'use strict';
 
-angular.module('scApp.wallet', ['ngRoute'])
+module.exports = (angular, sockets) => {
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/wallet', {
-    templateUrl: 'wallet/wallet.html',
-    controller: 'WalletCtrl'
-  });
-}])
+    angular.module('scApp.wallet', ['ngRoute'])
 
-.controller('WalletCtrl', ['$scope', function($scope) {
+    .config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/wallet', {
+        template: require('wallet.html'),
+        controller: 'WalletCtrl'
+      });
+    }])
 
-	// Define core variables
-	$scope.balance				= null;
-	$scope.unconfirmedbalance	= null;
-	$scope.newAddress = '';
-	$scope.recipient = {
-		address: null,
-		amount: null
-	};
+    .controller('WalletCtrl', ['$scope', function($scope) {
 
-	// Construct socket object
-	var socketio = io();
+        // Define core variables
+        $scope.balance              = 70.00;
+        $scope.unconfirmedBalance   = 0;
+        $scope.address = '';
 
-	
-	// Return elapsed time between now and value
-	$scope.elapsedTime = function(value) {
-		var nowTime = new Date().getTime();
-		var dateConvert = new Date(value * 1000);
-		dateConvert = new Date(nowTime - dateConvert);
-		return dateConvert;
-	}
-	$scope.formatDate = function(date) {
-		var hours = date.getHours();
-		var mins = date.getMinutes();
-		var seconds = date.getSeconds();
-		return (hours.toString().length == 1 ? "0" + hours : hours) + ":" + 
-		       (mins.toString().length == 1 ? "0" + mins : mins) + ":" + 
-		       (seconds.toString().length == 1 ? "0" + seconds : seconds);
-	}
+        $scope.recipient = {
+            address: null,
+            amount: null
+        };
 
-	// Get a list of transactions on function call
-	$scope.getBalance = function() {
-		socketio.emit("getbalance");
-		console.log("Requested balance");
-	};
+        // Construct socket object
+        var socketio = io();
 
 
-	$scope.getNewAddress = function() {
-		socketio.emit("getNewAddress");
-		console.log("Request new Addresss")
-	}
+        // Get a list of transactions on function call
+        $scope.getBalance = function() {
+            socketio.emit("getbalance");
+            console.log("Requested balance");
+        };
 
 
 
-	setInterval(function() { $scope.$apply(); }, 1000);
+        $scope.getUnconfirmedBalance = function() {
+            socketio.emit("getUnconfirmedbalance");
+            console.log("Requested balance");
+        };
 
-}]);
+        $scope.getNewAddress = function() {
+            socketio.emit("getNewAddress");
+            console.log("Request new Addresss")
+        }
+
+        socketio.on('getbalance', function(data) {
+            console.log("Received account balance");
+            console.log(data);
+            $scope.balance = data;
+            $scope.$apply();
+        });
+
+
+        socketio.on('getUnconfirmedbalance', function(data) {
+            console.log("Received account unconfirmed balance");
+            console.log(data);
+            $scope.unconfirmedBalanceBalance = data;
+            $scope.$apply();
+        });
+
+        setInterval(function() { $scope.$apply(); }, 1000);
+    }]);
+
+};
